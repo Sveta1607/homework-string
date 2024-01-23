ae7634c9a743a2f2d4d55f911fcfea60879fd244
Sveta <Sharunkina2014@yandex.ru>
2024-01-23 18:48:17 +0300

    decided first task

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+string GetStringFrmChar (char [,] array)
+{
+    string str = " ";
+    for (int i = 0; i < array.GetLength(0); i++)
+      for (int j = 0; j < array.GetLength(1); j++)
+    {
+      str += array[i,j];
+    }
+    return str;
+}
+
+char[,] charArray = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
+string res = GetStringFrmChar(charArray);
+Console.WriteLine((res));
